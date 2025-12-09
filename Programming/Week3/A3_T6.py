f15dcf89b5e3fb28171daeb24edf14dcf9eed1b3
Elman A. Emil <elman.a2617@gmail.com>
2025-12-09 19:15:24 +0200

    Update A3_T6.py

--- Programming/Week3/A3_T6.py
+++ Programming/Week3/A3_T6.py
@@ -1,54 +1,66 @@
 print("Program starting.")
+print("Welcome to the unit converter program!\nFollow the menu instructions below.")
 print()
-
-print("Main Menu:")
-print("1 -- Length")
-print("2 -- Weight")
-print("3 -- Exit")
+print("Options:")
+print("1 - Length")
+print("2 - Weight")
+print("0 - Exit")
 
 choice = int(input("Your choice: "))
 
 if choice == 1:
     print()
-    print("Length conversions:")
-    print("1 -- Meters to Kilometers")
-    print("2 -- Kilometers to Meters")
-    sub_choice = int(input("Your choice: "))
-
-    if sub_choice == 1:
-        meters = float(input("Insert the amount of meters: "))
-        km = meters / 1000
-        print(f"{round(meters,1)} m equals to {round(km,1)} km")
-    elif sub_choice == 2:
-        km = float(input("Insert the amount of kilometers: "))
-        meters = km * 1000
-        print(f"{round(km,1)} km equals to {round(meters,1)} m")
-    else:
+    print("Length options:")
+    print("1 - meter to kilometer")
+    print("2 - kilometer to meter")
+    print("0 - exit")
+    
+    choice1 = int(input("Your choice: "))
+    
+    if choice1 == 1:
+        meter = float(input("Insert meter: "))
+        kilometer = round(meter / 1000, 1)
+        print(f"{meter} m is {kilometer} km")
+        
+    elif choice1 == 2:
+        kilometer = float(input("Insert kilometer: "))
+        meter = round(kilometer * 1000, 1)
+        print(f"{kilometer} km is {meter} m")
+        
+    elif choice1 == 0:
         print("Exiting...")
-
-elif choice == 2:
-    print()
-    print("Weight conversions:")
-    print("1 -- Grams to Pounds")
-    print("2 -- Pounds to Grams")
-    sub_choice = int(input("Your choice: "))
-
-    if sub_choice == 1:
-        grams = float(input("Insert the amount of grams: "))
-        pounds = grams / 453.59237
-        print(f"{round(grams,1)} g equals to {round(pounds,1)} lb")
-    elif sub_choice == 2:
-        pounds = float(input("Insert the amount of pounds: "))
-        grams = pounds * 453.59237
-        print(f"{round(pounds,1)} lb equals to {round(grams,1)} g")
+        
     else:
         print("Unknown option.")
 
-elif choice == 3:
+elif choice == 2:
+    print("\nWeight options:")
+    print("1 - grams to pounds")
+    print("2 - pounds to grams")
+    print("0 - Exit")
+    
+    choice1 = int(input("Your choice: "))
+    
+    if choice1 == 1:
+        grams = float(input("Insert grams: "))
+        pounds = round(grams / 453.592, 1)
+        print(f"{grams} g is {pounds} lb")
+        
+    elif choice1 == 2:
+        pounds = float(input("Insert pounds: "))
+        grams = round(pounds * 453.592, 1)
+        print(f"{pounds} lb is {grams} g")
+        
+    elif choice1 == 0:
+        print("Exiting...")
+        
+    else:
+        print("Unknown option.")
+
+elif choice == 0:
     print("Exiting...")
 
 else:
     print("Unknown option.")
-
 print()
 print("Program ending.")
